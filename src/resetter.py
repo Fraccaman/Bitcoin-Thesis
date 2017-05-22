7b8307308c7362fc01933598974511470f146678
Fraccaman <gianmarco.fraccaroli@gmail.com>
2017-05-22 17:10:11 +0200

    New copy file

--- src/resetter.py
+++ src/resetter.py
@@ -1,4 +1,4 @@
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool as ThreadPool
 import time
 import shutil
 import os
@@ -46,6 +46,58 @@ def copytree(src, dst, symlinks=False, ignore=None, isRoot = True):
         except Exception as err:
             errors.extend(err.args[0])
 
+class CTError(Exception):
+    def __init__(self, errors):
+        self.errors = errors
+
+try:
+    O_BINARY = os.O_BINARY
+except:
+    O_BINARY = 0
+READ_FLAGS = os.O_RDONLY | O_BINARY
+WRITE_FLAGS = os.O_WRONLY | os.O_CREAT | os.O_TRUNC | O_BINARY
+BUFFER_SIZE = 128*1024
+
+def copyfile(src, dst):
+    try:
+        fin = os.open(src, READ_FLAGS)
+        stat = os.fstat(fin)
+        fout = os.open(dst, WRITE_FLAGS, stat.st_mode)
+        for x in iter(lambda: os.read(fin, BUFFER_SIZE), ""):
+            os.write(fout, x)
+    finally:
+        try: os.close(fin)
+        except: pass
+        try: os.close(fout)
+        except: pass
+
+def copytree2(src, dst, symlinks=False, ignore=[]):
+    names = os.listdir(src)
+
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    errors = []
+    for name in names:
+        if name in ignore:
+            continue
+        srcname = os.path.join(src, name)
+        dstname = os.path.join(dst, name)
+        try:
+            if symlinks and os.path.islink(srcname):
+                linkto = os.readlink(srcname)
+                os.symlink(linkto, dstname)
+            elif os.path.isdir(srcname):
+                copytree(srcname, dstname, symlinks, ignore)
+            else:
+                copyfile(srcname, dstname)
+            # XXX What about devices, sockets etc.?
+        except (IOError, os.error), why:
+            errors.append((srcname, dstname, str(why)))
+        except CTError, err:
+            errors.extend(err.errors)
+    if errors:
+        raise CTError(errors)
+
 def removeAndCopy(path):
   print("Starting copy of " + str(path))
   backup = os.path.expanduser("~") + '/../bitcoinbackup/bitcoin/'
@@ -62,7 +114,7 @@ def removeAndCopy(path):
     except Exception as e:
         print(e)
 
-  copytree(backup, path)
+  copytree2(backup, path)
 
 class Timer():
 
